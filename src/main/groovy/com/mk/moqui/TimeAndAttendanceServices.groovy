cfb9c9f05d6030e87adb44f9712b9da830ccce45
jomar <jomar_mendoza10@yahoo,.com>
2020-01-14 14:46:35 -0800

    config moqui to run groovy file

---
+++ src/main/groovy/com/mk/moqui/TimeAndAttendanceServices.groovy
@@ -0,0 +1,8 @@
+package com.mk.moqui
+import org.moqui.context.ExecutionContext
+
+public class TimeAndAttendanceServices {
+    public static void calculateSumOfHours(ExecutionContext ec){
+        System.out.println("=============================test===============================")
+    }
+}
