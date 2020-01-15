0fa4ecebc2dcae4fb91e359c2925dad79c019963
jomar <jomar_mendoza10@yahoo,.com>
2020-01-15 15:31:10 -0800

    Update the timeEntry to get over time hours

--- src/main/groovy/com/mk/moqui/TimeAndAttendanceServices.groovy
+++ src/main/groovy/com/mk/moqui/TimeAndAttendanceServices.groovy
@@ -1,8 +1,57 @@
 package com.mk.moqui
 import org.moqui.context.ExecutionContext
+import org.moqui.entity.EntityValue
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.text.DecimalFormat
+import java.sql.Timestamp
+
 
 public class TimeAndAttendanceServices {
-    public static void calculateSumOfHours(ExecutionContext ec){
-        System.out.println("=============================test===============================")
+    static Map calculateSumOfHours(ExecutionContext ec) {
+        double sumOfHours = 0
+        String dayOfTheWeek = ec.context.day
+        Boolean overTime = ec.context.overTime
+        int arraySize = ec.context.timeEntryList.size()
+        int days = ec.context.week.toInteger()
+
+        Calendar c = GregorianCalendar.getInstance()
+
+        c.set(Calendar.DAY_OF_WEEK, Calendar[dayOfTheWeek])
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        String startDate = "", endDate = ""
+
+        startDate = df.format(c.getTime()) + " 00:00:00.000"
+        c.add(Calendar.DATE, days)
+        endDate = df.format(c.getTime()) + " 23:59:59.999"
+
+        SimpleDateFormat dateFormatForTS = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS")
+        Date parsedStartDate = dateFormatForTS.parse(startDate)
+        Timestamp timeStampStart = new java.sql.Timestamp(parsedStartDate.getTime())
+        Date parsedEndDate = dateFormatForTS.parse(endDate)
+        Timestamp timeStampEnd = new java.sql.Timestamp(parsedEndDate.getTime())
+
+        for (int i =0; i < arraySize; i++) {
+            EntityValue ev = ec.context.timeEntryList[i]
+
+            if(ev.getPlainValueMap(0).fromDate > timeStampStart && ev.getPlainValueMap(0).thruDate < timeStampEnd && ev.getPlainValueMap(0).hours != null) {
+                sumOfHours = sumOfHours + ev.getPlainValueMap(0).hours
+            }
+        }
+        System.out.println(sumOfHours + " before the check")
+        if(sumOfHours > 8 && overTime == true){
+            sumOfHours -= 8
+        }
+        System.out.println(sumOfHours + " after the check")
+
+        DecimalFormat df2 = new DecimalFormat("###.##")
+        String result = "sumOfHours"
+        result = result.concat(days.toString())
+        def totalHours = [:]
+        totalHours[result] = df2.format(sumOfHours)
+        return totalHours
     }
+    
 }
+
